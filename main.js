78aa7ffb4bf479fc622f5ef8c82e6430e890a7b4
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-04-26 01:13:24 -0700

    added 5day forecast

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const api = {
 };
 
 const searchBox = document.querySelector(".search-box");
+const forecastDiv = document.querySelector(".forecast-div");
 searchBox.addEventListener("keypress", setQuery);
 
 function setQuery(e) {
   if (e.keyCode == 13) {
+    forecastDiv.innerHTML = "";
     getResults(searchBox.value);
+    getForecast(searchBox.value);
   }
 }
 
@@ -18,6 +21,18 @@ function getResults(query) {
       return weather.json();
     })
     .then(displayResults);
+}
+
+function getForecast(query) {
+  fetch(`${api.baseurl}forecast?q=${query}&units=imperial&APPID=${api.key}`)
+    .then((forecast) => {
+      return forecast.json();
+    })
+    .then((forecast) => {
+      let city = forecast.city.name;
+      let dailyForecast = forecast.list;
+      displayForecast(city, dailyForecast);
+    });
 }
 
 function getWeatherIcon(weather) {
@@ -34,8 +49,60 @@ function getWeatherIcon(weather) {
   }
 }
 
+function applyIcon(icon) {
+  if (icon === "01d") {
+    return "wi-day-sunny";
+  } else if (icon === "01n") {
+    return "wi-night-clear";
+  } else if (icon === "02d" || "02n") {
+    return "wi-cloudy";
+  } else if (icon === "03d" || "03n" || "04d" || "04n") {
+    return "wi-night-cloudy";
+  } else if (icon === "09d" || "09n") {
+    return "wi-showers";
+  } else if (icon === "10d" || "10n") {
+    return "wi-rain";
+  } else if (icon === "11d" || "11n") {
+    return "wi-thunderstorm";
+  } else if (icon === "13d" || "13n") {
+    return "wi-snow";
+  } else if (icon === "50d" || "50n") {
+    return "wi-fog";
+  } else {
+    // whoa, destruction!
+    return "wi-meteor";
+  }
+}
+
+function displayForecast(location, forecast) {
+  console.log(forecast);
+  // let fIcon = getForecastIcon(forecast);
+
+  const forecastDiv = document.querySelector(".forecast-div");
+
+  forecast.forEach((day) => {
+    let date = new Date(day.dt * 1000);
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    let name = days[date.getDay()];
+    // console.log(day.weather[0].main);
+    let dayBlock = document.createElement("div");
+    dayBlock.className = "forecast__item";
+    dayBlock.innerHTML = `<div class="forecast-item__heading">${name} ${
+      day.dt_txt
+    }</div>
+      <div class="forecast-item__info">
+      <i class="wi ${applyIcon(day.weather[0].icon)}"></i>
+      <span class="degrees">${Math.round(day.main.temp)}
+      <i class="wi wi-degrees"></i></span></div>`;
+    forecastDiv.appendChild(dayBlock);
+  });
+}
+
 function displayResults(weather) {
+  const wDescription = weather.weather[0].description;
+
   console.log(weather);
+
   backgroundChange(weather);
   let wIcon = getWeatherIcon(weather);
   let city = document.querySelector(".location .city");
@@ -46,10 +113,10 @@ function displayResults(weather) {
   date.innerText = dateBuilder(now);
 
   let temp = document.querySelector(".current .temp");
-  temp.innerHTML = `${Math.round(weather.main.temp)} <span>°F</span>`;
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
 
   let weatherEl = document.querySelector(".current .weather");
-  weatherEl.innerHTML = `${wIcon}${weather.weather[0].main}`;
+  weatherEl.innerHTML = `${wIcon}${wDescription}`;
 
   let hiLow = document.querySelector(".hi-low");
   hiLow.innerText = `${Math.round(weather.main.temp_min)}°F - ${Math.round(
@@ -93,12 +160,12 @@ function dateBuilder(d) {
 function backgroundChange(weather) {
   const wType = weather.weather[0].main;
   if (wType == "Rain") {
-    document.body.style.backgroundImage = "url('rainy.jpg')";
+    document.body.style.backgroundImage = "url('/css/rainy.jpg')";
   } else if (wType == "Clouds") {
-    document.body.style.backgroundImage = "url('cloudy.jpg')";
+    document.body.style.backgroundImage = "url('/css/cloudy.jpg')";
   } else if (wType == "Clear") {
-    document.body.style.backgroundImage = "url(clear.jpg)";
+    document.body.style.backgroundImage = "url(/css/clear.jpg)";
   } else {
-    document.body.style.backgroundImage = "url(weather-background.jpeg)";
+    document.body.style.backgroundImage = "url(/css/weather-background.jpeg)";
   }
 }
